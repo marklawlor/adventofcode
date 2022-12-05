55ed880136bb3006e3b64c44c5e7c1cd39bc9bec
Mark Lawlor <mwlawlor@gmail.com>
2022-12-05 16:23:20 +1000

    Day 05

---
+++ 2022/05/a.ts
@@ -0,0 +1,49 @@
+const sampleInput = `    [D]    
+[N] [C]    
+[Z] [M] [P]
+ 1   2   3 
+
+move 1 from 2 to 1
+move 3 from 1 to 3
+move 2 from 2 to 1
+move 1 from 1 to 2`;
+
+const sampleSolution = "CMZ";
+
+const actualInput = await Deno.readTextFile(
+  new URL("./input.txt", import.meta.url),
+);
+
+function answer(input = actualInput) {
+  const lines = input.split("\n").filter(Boolean)
+
+  const columns = [];
+
+  let line = lines.shift();
+
+  while (!line.startsWith(" 1 ")) {
+    for (let i = 1;i<=line.length;i+=4) {
+      if (line[i] === " ") continue
+      const index = (i - 1) / 4
+      columns[index] ??= [];
+      columns[index].unshift(line[i])
+    }
+    line = lines.shift();
+  }
+
+  for (const line of lines) {
+    const [, amountString, , fromString, , toString] = line.split(" ");
+    const amount = Number.parseInt(amountString, 10)
+    const from = Number.parseInt(fromString, 10) - 1
+    const to = Number.parseInt(toString, 10) - 1
+
+    for (let i = amount; i != 0; i--) {
+      const take = columns[from].pop()
+      columns[to].push(take)
+    }
+  }
+
+  return columns.map((col) => col.pop()).join("");
+}
+
+console.log("a: ", answer(sampleInput) === sampleSolution, answer());
---
+++ 2022/05/b.ts
@@ -0,0 +1,47 @@
+const sampleInput = `    [D]    
+[N] [C]    
+[Z] [M] [P]
+ 1   2   3 
+
+move 1 from 2 to 1
+move 3 from 1 to 3
+move 2 from 2 to 1
+move 1 from 1 to 2`;
+
+const sampleSolution = "MCD";
+
+const actualInput = await Deno.readTextFile(
+  new URL("./input.txt", import.meta.url),
+);
+
+function answer(input = actualInput) {
+  const lines = input.split("\n").filter(Boolean)
+
+  const columns = [];
+
+  let line = lines.shift();
+
+  while (!line.startsWith(" 1 ")) {
+    for (let i = 1;i<=line.length;i+=4) {
+      if (line[i] === " ") continue
+      const index = (i - 1) / 4
+      columns[index] ??= [];
+      columns[index].unshift(line[i])
+    }
+    line = lines.shift();
+  }
+
+  for (const line of lines) {
+    const [, amountString, , fromString, , toString] = line.split(" ");
+    const amount = Number.parseInt(amountString, 10)
+    const from = Number.parseInt(fromString, 10) - 1
+    const to = Number.parseInt(toString, 10) - 1
+
+    const take = columns[from].splice(amount * -1, amount)
+    columns[to].push(...take)
+  }
+
+  return columns.map((col) => col.pop()).join("");
+}
+
+console.log("b: ", answer(sampleInput) === sampleSolution, answer());
