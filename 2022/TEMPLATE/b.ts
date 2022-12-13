ede4d7d05f056b48943eafc2f8ac455df3b02742
Mark Lawlor <mwlawlor@gmail.com>
2022-12-13 10:06:54 +1000

    update template

--- 2022/TEMPLATE/b.ts
+++ 2022/TEMPLATE/b.ts
@@ -3,11 +3,17 @@ const sampleInput = ``;
 const sampleSolution = 0;
 
 const actualInput = await Deno.readTextFile(
-  new URL("./input.txt", import.meta.url),
+  new URL("./input.txt", import.meta.url)
 );
 
 function answer(input = actualInput) {
+  const lines = input.trim().split("\n");
   return false;
 }
 
-console.log("b: ", answer(sampleInput) === sampleSolution, answer());
+console.log(
+  "b: ",
+  answer(sampleInput),
+  answer(sampleInput) === sampleSolution,
+  answer()
+);
